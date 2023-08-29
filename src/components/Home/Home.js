6e8b72c72d9d9b39ba660b0070ef9acd91545a3c
oshin pojta <sujanian785@gmail.com>
2023-08-29 13:40:25 +0530

    fixed ui issues

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,6 +18,15 @@ const Home = () => {
         <h3>
           An elegant website to showcase responsive design. If you checkout this website on different displays or even when decreasing browser-window width, it displays smoth transition between movie-grid items.
           Added advanced features like Top-Loader and Infinite Scroll which makes user-experience even better.
+          <ul className="home-list">
+            <li>1. Added Search Bar for searching based on title, plot, cast, directors, genres, languages etc</li>
+            <li>2. Use of webhooks like useState, useEffect, useRef, useContext </li>
+            <li>3. Added features like setting offset and limit for pagination</li>
+            <li>4. Advanced UI like Top-Loader and Infinite-Scroll</li>
+            <li>5. Elegant card responsive-ui</li>
+            <li>6. Enjoy this piece of Art ...!!!</li>
+          </ul>
+          
         </h3>
         <br />
         <h3>
